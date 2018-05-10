dd79bb82de9e3dd4e9bd41fbf5ad7fa91270fa95
kAbhi <sharmabhishek134@gmail.com>
2018-05-10 14:28:14 +0530

    adding in-out gesture

--- WebApp/scripts/events.js
+++ WebApp/scripts/events.js
@@ -6,22 +6,61 @@ var controller = Leap.loop(options, function(frame){
 function swipeHandler(gesture){
     //Classify swipe as either horizontal or vertical
     console.log(gesture);
-    var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
 
-    if (isHorizontal) {
+    // var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+
+    var compare01 = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+    var compare02 = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[2]);
+    var compare12 = Math.abs(gesture.direction[1]) > Math.abs(gesture.direction[2]);
+
+    if (compare01 && compare02) {
+      // left - right
       if (gesture.direction[0] > 0) {
+        // right
         swipeDirection = "right";
-      } else {
+      }
+      else {
+        // left
         swipeDirection = "left";
       }
-    } else {
-      // vertical
-      if(gesture.direction[1] > 0){
+    }
+    else if (!compare01 && compare12) {
+      // up - down
+      if (gesture.direction[1] > 0) {
+        // up
         swipeDirection = "up";
-      } else {
-          swipeDirection = "down";
+      }
+      else {
+        // down
+        swipeDirection = "down";
       }
     }
+    else if (!compare02 && !compare12) {
+      // in - out
+      if (gesture.direction[2] > 0) {
+        // out
+        swipeDirection = "out";
+      }
+      else {
+        // in
+        swipeDirection = "in";
+      }
+    }
+
+    // if (isHorizontal) {
+    //   if (gesture.direction[0] > 0) {
+    //     swipeDirection = "right";
+    //   } else {
+    //     swipeDirection = "left";
+    //   }
+    // } else {
+    //   // vertical
+    //   if(gesture.direction[1] > 0){
+    //     swipeDirection = "up";
+    //   } else {
+    //       swipeDirection = "down";
+    //   }
+    // }
     console.log(swipeDirection);
 }
 controller.on("gesture", function(gesture){
