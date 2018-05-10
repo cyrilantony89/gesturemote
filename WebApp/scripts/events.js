a5f3c963ae8f10815441a56c1d22ca004ff14a51
seal-7 <rajtalekar24@gmail.com>
2018-05-10 07:41:14 -0400

    Implemented left right gesture with server calls

--- WebApp/scripts/events.js
+++ WebApp/scripts/events.js
@@ -1,13 +1,73 @@
-alert("STARTING");
+console.log("Start events");
+//Setting up Leap Motion
 var options={enableGestures: true};
 var controller = Leap.loop(options, function(frame){
   //... handle frame data
 });
-function swipeHandler(gesture){
-    //Classify swipe as either horizontal or vertical
-    console.log(gesture);
-    var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
 
+//Validator's getter and setters
+var Validator = (function(){
+  var leftOrRight = 0;
+  var newGesture = true;
+  var getLeftOrRight = function(){
+    return (leftOrRight<0) ? "left" : "right";
+  }
+  var resetLeftOrRight = function(){
+    leftOrRight = 0;
+  }
+  var setLeftOrRight = function(gesture){
+    if(gesture == "left"){
+      leftOrRight --;
+    }
+    else{
+      leftOrRight ++;
+    }
+  }
+  var isNewGesture =function(){
+      return newGesture;
+  }
+  var setNewGesture = function(value){
+    newGesture = value;
+  }
+  return {
+    getLeftOrRight : getLeftOrRight,
+    resetLeftOrRight : resetLeftOrRight,
+    setLeftOrRight : setLeftOrRight,
+    isNewGesture : isNewGesture,
+    setNewGesture : setNewGesture
+  }
+}());
+
+
+
+function makeCall(swipeDirection){
+  var server = "http://10.177.65.71:8080/server/changechannel";
+  var payload={
+    direction : swipeDirection
+  }
+  var call=$.ajax({
+        type: 'POST',
+        url: server,
+        data: JSON.stringify(payload),
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function(resultData) {
+           console.log(JSON.stringify(resultData));
+        },
+        error: function(error){
+          console.error(error);
+        }
+      });
+
+}
+function timerfunc(){
+  makeCall(Validator.getLeftOrRight());     //makeCall for left or right direction
+  Validator.resetLeftOrRight();             //reSetting Left or Right to 0
+  Validator.setNewGesture(true);            //Now upcoming gestures will be new gesture
+}
+function swipeHandler(gesture){
+
+    var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
     if (isHorizontal) {
       if (gesture.direction[0] > 0) {
         swipeDirection = "right";
@@ -15,14 +75,20 @@ function swipeHandler(gesture){
         swipeDirection = "left";
       }
     } else {
-      // vertical
       if(gesture.direction[1] > 0){
         swipeDirection = "up";
       } else {
           swipeDirection = "down";
       }
     }
-    console.log(swipeDirection);
+    //console.log(swipeDirection + " " + Math.floor(gesture.duration/1000000));
+    if ( (swipeDirection == "right" || swipeDirection=="left") && Validator.isNewGesture() ) {
+      console.log("New Guesture");
+      setTimeout(timerfunc,500);   //millisceconds
+      Validator.setNewGesture(false);     //all upcoming gestures will not be new until reset.
+    }
+    Validator.setLeftOrRight(swipeDirection);
+  //  console.log(gesture.duration);
 }
 controller.on("gesture", function(gesture){
   switch (gesture.type){
